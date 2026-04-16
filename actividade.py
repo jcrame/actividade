fb358b23ad558ab7c3fee7e60eeed9f80ff62cce
jcrame <juan31207553@gmail.com>
2026-04-15 21:52:02 -0300

    Add files via upload

---
+++ actividade.py
@@ -0,0 +1,38 @@
+nome = input("coloque teu nome: ")
+idade = input("adicione tua idade: ")
+cidade = input("de qual cidade é vc: ")
+
+print(f"mi nombre es {nome} y tengo {idade} años y soy de {cidade}")
+
+
+
+
+print ("pode colocar suas notas")
+
+nota1 = float(input("nota #1: "))
+nota2 = float(input("nota #2: "))
+
+suma = nota1 + nota2
+print (f'nota1 + nota2 = {suma}')
+
+subtracao = nota1 - nota2
+print (f'nota1 - nota2 = {subtracao}')
+
+multiplicacao = nota1*nota2
+print (f'nota1 x nota2 = {multiplicacao}')
+
+divisao = nota1 / nota2
+print (f'nota1 / nota2 = {divisao}')
+
+
+
+
+numero = int(input("coloque numero: "))
+
+for i in range(1,11):
+    result = i*numero
+    print(f'{i} x {numero} = {result}' ) 
+
+
+
+    
