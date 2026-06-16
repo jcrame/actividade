d04a11842b92442c1d10549adf66ad22a795bbda
jcrame <juan31207553@gmail.com>
2026-06-15 23:25:30 -0300

    Add ticket sales calculation for a movie

--- recuperação/#1.py
+++ recuperação/#1.py
@@ -1,1 +1,7 @@
-mmm
+f = input('Digite o filme: ')
+p = int (input ('coloque o preço unitario:R$ '))
+q = int (input('quantos ingressos foi vendidos: '))
+
+v_total =  (p) * (q) 
+
+print( f'Filme: {f} | Preço unitário:{p}  | Quantidade:{q} | Total: R$ {v_total} ' )
